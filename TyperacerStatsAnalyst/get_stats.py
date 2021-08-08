b545ccf784b1b590b9b0de3c33bad10dc4ada095
kinfi4 <ilyamakarov223@gmail.com>
2021-08-08 09:26:46 +0300

    parsing finished

--- TyperacerStatsAnalyst/get_stats.py
+++ TyperacerStatsAnalyst/get_stats.py
@@ -1,5 +1,11 @@
 from project_files.parser import Parser
 
 
-parser_ = Parser()
-parser_.parse_user_stats('kinfi4')
+parser = Parser()
+stats = parser.parse_user_stats('kinfi4')
+
+print(len(stats.user_stats))
+print(stats.user_stats)
+stats.save_to_file('mystats.csv')
+# print(stats.user_stats[1])
+# print(stats.user_stats[2])
--- TyperacerStatsAnalyst/project_files/parser.py
+++ TyperacerStatsAnalyst/project_files/parser.py
@@ -1,3 +1,4 @@
+import sys
 import csv
 import re
 from string import Template
@@ -16,16 +17,39 @@ class Statistics:
         with open(filepath, 'w') as file:
             writer = csv.DictWriter(file, self.user_stats.pop(0))
 
-            for row in self.user_stats:
-                writer.writerow(row)
+            writer.writerows(self.user_stats)
+            # for row in self.user_stats:
+            #     print(row)
+            #     writer.writerow(row)
 
     def append(self, data):
         self.user_stats.append(data)
 
+    def __str__(self):
+        return f'Stats: {self.user_stats[0]}'
+
 
 class Parser:
     def __init__(self):
-        pass
+        self.user_data = []
+
+    def parse_user_stats(self, username) -> Statistics:
+        url, next_cursor = Template(URL), ''
+
+        self.user_data.append(self._parse_table_head(url.substitute({'user': username, 'cursor': ''})))
+        counter = 0
+
+        while True:
+            data, next_cursor = self._parse_single_page(url.substitute({'user': username, 'cursor': next_cursor}))
+            self.user_data.extend(data)
+            counter += 1
+
+            print(f'Page {counter} parsed\r', flush=True, end='')
+
+            if not next_cursor:  # we have reached the last table
+                break
+
+        return Statistics(self.user_data)
 
     def _parse_single_page(self, url):
         page = requests.get(url)
@@ -34,25 +58,33 @@ class Parser:
 
         rows = stats_table.find_all('tr')
 
-        data = [tuple(element.get_text(strip=True) for element in rows[0].find_all('th')[:-1])]  # here we use
-        # [:-1] because I dont want to have the last column options in my stats
-
-        for row in rows[1:]:
-            data.append(tuple(element.get_text(strip=True) for element in row.find_all('td')[:-1]))  # and here [:-1] as well
+        data = []
+        for row in rows[1:]:  # 1: so we skip the first row, of th
+            data.append(tuple(element.get_text(strip=True) for element in row.find_all('td')[:-1]))  # [:-1] so we dont get the last column
 
         next_cursor = self._find_next_cursor(page_soup)
 
         return data, next_cursor
 
     @staticmethod
+    def _parse_table_head(url):
+        page = requests.get(url)
+        rows = BeautifulSoup(page.text, 'html.parser').find('table', {'class': 'scoresTable'}).find_all('tr')
+
+        return tuple(element.get_text(strip=True) for element in rows[0].find_all('th')[:-1])  # here we use
+        # [:-1] because I dont want to have the last column "options" in my stats
+
+    @staticmethod
     def _find_next_cursor(page_soup):
-        next_link = page_soup.find('table', {'class': 'scoresTable'}).findNextSibling('div').find_all('span')[-1].find('a')
+        links = page_soup.find('table', {'class': 'scoresTable'}).findNextSibling('div').find_all('a')
+
+        next_link = None
+        for link in links:
+            if 'load older' in link.string:
+                next_link = link
+
+        if next_link is None:  # This means that we've reached the last table
+            return ''
+
         href = next_link.attrs.get('href')
         return re.search(r'cursor=(.*?)&', href).groups()[0]
-
-    def parse_user_stats(self, username) -> Statistics:
-        url = Template(URL)
-        data, next_cursor = self._parse_single_page(url.substitute({'user': username, 'cursor': ''}))
-        print(next_cursor)
-
-
