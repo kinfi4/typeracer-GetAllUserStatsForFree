5ace1db8fd461e3031c555e509d96a0a7065a53e
Illia Makarov <illia_makarov@epam.com>
2022-07-25 18:38:32 +0300

    Refactored logic, actions moved to a seperate file

--- TyperacerStatsAnalyst/get_stats.py
+++ TyperacerStatsAnalyst/get_stats.py
@@ -1,30 +1,7 @@
 import argparse
-from typing import Optional
-from datetime import datetime, date
+from datetime import datetime
 
-from src.parser import Parser
-from src.statistics_analysis import StatisticsVisualizer
-
-
-def parse(
-    username: str,
-    output_file: str,
-    show_plots: bool = True,
-    start_date: Optional[date] = None,
-    end_date: Optional[date] = None
-) -> None:
-    parser = Parser(start_date, end_date)
-    stats = parser.parse_user_stats(username)
-    stats.save_to_file(output_file)
-
-    if show_plots:
-        stats.plot_everything()
-
-
-def plot_stats_from_file(filepath: str, start_date: Optional[date] = None, end_date: Optional[date] = None):
-    stats = StatisticsVisualizer.get_stats_from_csv(filepath, start_date, end_date)
-
-    stats.plot_everything()
+from src.actions import parse, plot_stats_from_file
 
 
 if __name__ == '__main__':
